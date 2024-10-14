d6043f9c67718e58a09347289e8cbc782528d237
shiva <shivangishree020@gmail.com>
2024-10-14 11:31:22 +0530

    send message to future self

--- future-you-b/routes/dataRoute.js
+++ future-you-b/routes/dataRoute.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const data = require('../models/data');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
 router.post('/schedule', async (req, res) => {
@@ -13,6 +15,34 @@ router.post('/schedule', async (req, res) => {
         });
         await newData.save();
 
+        const dateToSchedule = new Date(scheduledDate);
+
+        const cronExpression = `${dateToSchedule.getMinutes()} ${dateToSchedule.getHours()} ${dateToSchedule.getDate()} ${dateToSchedule.getMonth() + 1} *`;
+
+        cron.schedule(cronExpression, async () => {
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.EMAIL_USER,
+                  pass: process.env.EMAIL_PASS,
+                },
+            });
+              
+            const mailOptions = {
+                from: process.env.EMAIL_USER,
+                to: email,
+                subject: 'Dear Self',
+                text: message || 'This is a scheduled message',
+            };
+              
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+             });
+        })
         res.status(200).json({ message: 'Email scheduled successfully!' });
     } catch (error) {
         console.error('Failed to schedule the message:', error); // Log error details
